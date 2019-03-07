1927cfd50e7d945e51b65df9dfe499219fcda92a
dodotronix <pacnerpetr@volny.cz>
2019-03-07 17:01:44 +0100

    added parameters for repeated signal

--- analysis/simul/xcorrela.m
+++ analysis/simul/xcorrela.m
@@ -11,13 +11,15 @@
 %          fs -->|            |
 %  signal_ret -->|  XCORRELA  |--> [xcorr_fun, d]
 %  signal_ref -->|            |
+%     repetit -->|            |
+%     bitrate -->|            |
 %          vf -->|____________|
 %
 % need below package for proper working
 % pkg load signal
 %
 
-function [xcorr_fun, d] = xcorrela(signal_ref, signal_ret, fs, vf)
+function [x_out, d] = xcorrela(signal_ref, signal_ret, fs, repetit, vf, bitrate)
   Ts = 1/fs; % signal period [s]
   c = 3e8; % speed of light [m/s]
 
@@ -26,10 +28,16 @@ function [xcorr_fun, d] = xcorrela(signal_ref, signal_ret, fs, vf)
   endif
 
   % correlate - length is 2-times larger vector
-  xcorr_fun = xcorr(signal_ret, signal_ref); % (signal_ret = static)
-  
-  % generate distance vector
-  bound = (length(xcorr_fun)-1)/2;
-  d = [-1*bound:bound]*Ts*c*vf/2;
+  x_out = xcorr(signal_ret, signal_ref); % (signal_ret = static)
 
+  if(repetit > 1)
+    % select first part of correlated signal without changeovers
+    bit_w = floor(fs/bitrate);
+    delta = length(signal_ret) + length(signal_ref)-bit_w;
+    x_out = x_out(delta:length(signal_ref)+delta);
+    d = [-bit_w:length(x_out)-1-bit_w]*Ts*c*vf/2; % distance vector
+  else
+    bound = (length(x_out)-1)/2;
+    d = [-1*bound:bound]*Ts*c*vf/2;
+  end
 endfunction
