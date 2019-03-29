8b1829b46fb98ef81a85e4bb22dec47a66ee7053
dodotronix <pacnerpetr@volny.cz>
2019-03-29 23:43:04 +0100

    size of legend changes, colors and linewidth

--- analysis/sim/measure/reflections.m
+++ analysis/sim/measure/reflections.m
@@ -14,15 +14,15 @@ pkg load ltfat
 %% Module setups
 
 order     =  8;      % order of the PN - sequence [-]
-res_adc   =  4;      % adc resolution [b]
+res_adc   =  8;      % adc resolution [b]
 fs_dac    =  125e6;  % adc (dac) sampling frequency [Hz]
 bitrate   =  25e6;   % [b/s]
 bw_dac    =  50e6;   % dac bandwidth [Hz]
 range_adc =  1;      % adc voltage range [V]
-cable_len =  25;     % length of cable [m]
+cable_len =  50;     % length of cable [m]
 cable_att =  9;      % cable attenuation [dB/100m]
 SNR       =  10;     % Signal noise ratio [-]
-amp       =  1;      % signal stimulus amplitude [V]
+amp       =  0.5;      % signal stimulus amplitude [V]
 
 % antenna measured s11
 s11 = '../antenna/VNA/K8.dat';
@@ -46,7 +46,7 @@ S = amp*prbs_gen(order);
 %------------------------------------------------------------------------------%
 %% estimate peak positions
 a = power(amp,2)*(power(2, order)-2)*fs_dac/bitrate;
-d_aprox = xd_open(10:50);
+d_aprox = xd_open(10:75);
 g = power(10, d_aprox*-cable_att/1000);
 y = a*g;
 
@@ -54,38 +54,40 @@ y = a*g;
 %%Plot
 
 figure(1)
-plot(d_aprox, y, 'b', 'linewidth', 1)
+plot(d_aprox, y, 'k--', 'linewidth', 1)
 hold on
-plot(xd_open, xc_open, '-.p', 'linewidth', 1)
+plot(xd_short, xc_short, '-', 'linewidth', 3)
 hold on
-plot(xd_short, xc_short, '-o', 'linewidth', 1)
+plot(xd_open, xc_open, '-.', 'linewidth', 3)
 hold on
-plot(xd_ant, xc_ant, 'o-r', 'linewidth', 1)
+plot(xd_ant, xc_ant, '-', 'linewidth', 3)
 hold on
-plot(d_aprox, -y, 'b', 'linewidth', 1)
-xlim([-10, 50])
-ylabel('{\Large Korelační amplituda [-]}')
-xlabel('{\Large Vzdálenost [m]}')
+plot(d_aprox, -y, 'k--', 'linewidth', 1)
+xlim([-10, 80])
+ylabel('{Korelační amplituda [-]}')
+xlabel('{Vzdálenost [m]}')
 grid on
 
 %% plot exporting setups
 orient('landscape')
-h=get(gcf, "currentaxes");
-set(h, "fontsize", 16);
+h = legend({'   teoretický odhad',... 
+            '   vedení nakrátko',... 
+            '   vedení naprázdno',... 
+            '   anténa'},'Location','northeast');
+
+set (h, 'fontsize', 19, 'position', [0.65,0.70,0.25,0.22]);
+set(gca, 'fontsize', 19);
 grid on
 
-h = legend({'teoretický odhad', 'rozpojení', 'zkrat', 'anténa'},'Location','northeast');
-set (h, "fontsize", 16);
-
 %% Generate Latex
-target = '../../../doc/outputs/sim/'
-name = 'reflections.tex'
-name_inc = 'reflections-inc.eps'
+%target = '../../../doc/outputs/sim/'
+%name = 'reflections.tex'
+%name_inc = 'reflections-inc.eps'
 
-print(name, '-dtex');
+%print(name, '-dtex');
 
-path = strcat(target, name);
-path_inc = strcat(target, name_inc);
+%path = strcat(target, name);
+%path_inc = strcat(target, name_inc);
 
-movefile(name, path);
-movefile(name_inc, path_inc);
+%movefile(name, path);
+%movefile(name_inc, path_inc);
