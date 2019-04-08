455a14d5b49e4ffce695998abf90346228e2b986
dodotronix <pacnerpetr@volny.cz>
2019-04-08 18:57:35 +0200

    automatical find and launch bitstream

--- design/hw/RedPitaya/scripts/load_bs.sh
+++ design/hw/RedPitaya/scripts/load_bs.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
 REDPITAYA="10.0.4.41"
-BITSTREAM="$(echo $1 | sed -r 's/.*\/(.*.bit)$/\1/')"
-echo $BITSTREAM
+
+BITSTREAM="$(find ../ -name *.bit)"
+NAME="$(echo $BITSTREAM | sed -r 's/.*\/(.*.bit)$/\1/')"
+echo $NAME
+
 #sshpass -p 'root' scp $1 root@$REDPITAYA:/dev/xdevcfg
-sshpass -p 'root' scp $1 root@$REDPITAYA:/root/
-sshpass -p 'root' ssh root@$REDPITAYA "cat /root/$BITSTREAM > /dev/xdevcfg"
+sshpass -p 'root' scp $BITSTREAM root@$REDPITAYA:/root/
+sshpass -p 'root' ssh root@$REDPITAYA "cat /root/$NAME > /dev/xdevcfg"
 
 # link: https://stackoverflow.com/questions/305035/how-to-use-ssh-to-run-a-shell-script-on-a-remote-machine
 
